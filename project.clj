bd14eebe0fb6896af401db07e86bc45636b24d55
Markku Rontu <markku.rontu@iki.fi>
2015-10-13 11:21:40 +0300

    Bump tangle to 0.1.8

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [macroz/tangle "0.1.7"]
+                 [macroz/tangle "0.1.8"]
                  [org.clojure/data.json "0.2.6"]]
   )
