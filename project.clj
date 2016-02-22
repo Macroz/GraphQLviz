b98443d5792bdd5b1f8a8d56e32483bb3e7ce7b2
Markku Rontu <markku.rontu@iki.fi>
2016-02-22 20:13:09 +0200

    chore: bump clojure to 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/Macroz/graphqlviz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [macroz/tangle "0.1.8"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "2.1.0"]
